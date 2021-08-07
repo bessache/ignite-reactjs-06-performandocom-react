ed3324d7ede970febb976dfabd93237feadfd288
Renato Bessa <bessache@outlook.com>
2021-08-07 10:35:25 -0300

    commit aula criando o app de exemplo

---
+++ components/SearchResults.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/jsx-key */
+import { ProductItem } from "./ProductItem";
+
+interface SearchResultsProps {
+    results: Array<{
+        id: number;
+        price: number;
+        title: string
+    }>
+}
+
+
+export function SearchResults({results}:SearchResultsProps) {
+    return (
+        <div>
+            {results.map(product => {
+                return (
+                    <ProductItem product={product}/>
+                )
+            })}
+        </div>
+    );
+}
